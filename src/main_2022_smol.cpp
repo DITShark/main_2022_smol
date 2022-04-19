a6bb27bdeb45b21d44f5816e1421bd3a7caf68c9
DITShark <92559495+DITShark@users.noreply.github.com>
2022-04-19 23:26:51 +0800

    Add files via upload

--- src/main_2022_smol.cpp
+++ src/main_2022_smol.cpp
@@ -146,7 +146,7 @@ geometry_msgs::Pose2D next_correction;
 
 vector<mission> mission_List;
 vector<int> missionTime_correct_Type;
-vector<int> missionTime_correct_Num;
+vector<double> missionTime_correct_Num;
 
 // Function Define
 
@@ -458,7 +458,7 @@ int main(int argc, char **argv)
 
                 for (size_t i = 0; i < missionTime_correct_Type.size(); i++)
                 {
-                    ROS_INFO("Mission [%c] Correct to %d secs", missionTime_correct_Type[i], missionTime_correct_Num[i]);
+                    ROS_INFO("Mission [%c] Correct to %f secs", missionTime_correct_Type[i], missionTime_correct_Num[i]);
                 }
                 cout << endl;
 
